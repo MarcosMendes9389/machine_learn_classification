dd31beedfd925f186d7e8664a337d8b2f914c40f
MarcosMendes9389 <marcosmendes9389@gmail.com>
2018-12-26 20:45:32 -0300

    adding neural network mlpclassifier

---
+++ glass_classification_MLP.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+
+import pandas
+
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPClassifier
+
+# Load dataset
+url = "glass_data/glass.data"
+names = ['id', 'refractive index', 'Sodium', 'Magnesium', 'Aluminum', 'Silicon', 'Potassium', 'Calcium', 'Barium', 'Iron', 'Type']
+dataset = pandas.read_csv(url, names=names)
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:, 1:10]
+Y = array[:, 10]
+validation_size = 0.2
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+scoring = 'accuracy'
+
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+cv_results = model_selection.cross_val_score(MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+              beta_1=0.9, beta_2=0.999, early_stopping=False,
+              epsilon=1e-08, hidden_layer_sizes=(15,),
+              learning_rate='constant', learning_rate_init=0.001,
+              max_iter=200, momentum=0.9, n_iter_no_change=10,
+              nesterovs_momentum=True, power_t=0.5,  random_state=1,
+              shuffle=True, solver='lbfgs', tol=0.0001,
+              validation_fraction=0.1, verbose=False, warm_start=False), X_train, Y_train, cv=kfold, scoring=scoring)
+msg = "msg %s: %f (%f)" % ('MLPClassifier', cv_results.mean(), cv_results.std())
+print(msg)
+
+
+# Make predictions on validation dataset
+mlp = MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+              beta_1=0.8, beta_2=0.2, early_stopping=False,
+              epsilon=1e-08, hidden_layer_sizes=(15,),
+              learning_rate='constant', learning_rate_init=0.001,
+              max_iter=200, momentum=0.9, n_iter_no_change=10,
+              nesterovs_momentum=True, power_t=0.5,  random_state=1,
+              shuffle=True, solver='lbfgs', tol=0.0001,
+              validation_fraction=0.1, verbose=False, warm_start=False)
+mlp.fit(X_train, Y_train)
+predictions = mlp.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
