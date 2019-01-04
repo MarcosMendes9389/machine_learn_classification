841617e8ecdb18da87040e2c0c627f3cbace91b0
MarcosMendes9389 <marcosmendes9389@gmail.com>
2019-01-03 21:18:13 -0300

    changing parameters mlp

--- src/glass_classification_MLP.py
+++ src/glass_classification_MLP.py
@@ -24,14 +24,8 @@ X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
 # Test options and evaluation metric
 scoring = 'accuracy'
 
-mlp = MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
-              beta_1=0.8, beta_2=0.2, early_stopping=False,
-              epsilon=1e-08, hidden_layer_sizes=(15,),
-              learning_rate='constant', learning_rate_init=0.001,
-              max_iter=200, momentum=0.9, n_iter_no_change=10,
-              nesterovs_momentum=True, power_t=0.5,  random_state=1,
-              shuffle=True, solver='lbfgs', tol=0.0001,
-              validation_fraction=0.1, verbose=False, warm_start=False)
+#solver : {'lbfgs', 'sgd', 'adam'}, default 'adam'
+mlp = MLPClassifier(random_state=seed, solver='lbfgs')
 
 kfold = model_selection.KFold(n_splits=10, random_state=seed)
 cv_results = model_selection.cross_val_score(mlp, X_train, Y_train, cv=kfold, scoring=scoring)
